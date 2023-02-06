36d3723f191f0979ad4122d1090a8e31a314cd7c
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-02-06 22:26:40 +0300

    c++

---
+++ project2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    std::cout << "оатмоат" << std::endl;
+    std::cout << "оатмоат" << std::endl;
+    std::cout <<"Hello, world!" << std::endl;
+
+    int x;
+    x = 5;
+    cout << x << endl;
+
+    int a = 6;
+    a = a-3;
+    cout << a << endl;
+
+    int b =a;
+    cout << b << endl;
+
+    cout << a+ b << endl;
+
+    cout << a << endl;
+
+    int c;
+    cout << c << endl;
+
+    cout << "Enter a number: ";
+    int z=0;
+    cin >> a;
+    cout << "You entered " << a <<endl;
+
+    return 0;
+}
