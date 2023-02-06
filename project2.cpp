70e12c837a48aab1667e2b9c90ed800b448db201
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-02-06 22:56:10 +0300

    Function.The beginning

--- project2.cpp
+++ project2.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 
+void doPrint() {
+    std::cout << "In doPrunt()" << std::endl;
+}
+
+int print7() {
+    return 7;
+} 
+
+void ReturnNothing() {
+    std::cout << "Hi!" << std::endl;
+}
+
+int getValueFromUser() {
+    std::cout << "enter sn integer: ";
+    int x;
+    std::cin >> x;
+    return x;
+}
+
 int main() {
+
+    ReturnNothing();
+
+    int y = getValueFromUser();
+    int u = getValueFromUser();
+
+    std::cout << y << " + " << u << " = " << y+u << std::endl;
 
     using namespace std;
 
@@ -8,6 +34,13 @@ int main() {
     std::cout << "оатмоат" << std::endl;
     std::cout << "оатмоат" << std::endl;
     std::cout <<"Hello, world!" << std::endl;
+
+    std::cout << "Starting main" << std::endl;
+    doPrint();
+    std::cout << "Ending main" << std::endl;
+
+    int d = print7();
+    std::cout << "function reterned: " << d << endl;
 
     int x;
     x = 5;
